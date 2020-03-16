b391672eb6ba25a121986b85b94e057fd0f1b7b0
Prabha Dodda <doddaprabha@gmail.com>
2020-03-17 00:50:03 +0800

    Feature1-PasswordStrengthImplementation

--- ImageHoster_assignment_stub/ImageHoster/src/main/java/ImageHoster/controller/UserController.java
+++ ImageHoster_assignment_stub/ImageHoster/src/main/java/ImageHoster/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.regex.Pattern;
 
 
 @Controller
@@ -40,9 +41,37 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model) {
+            boolean alphabetExists = false;
+            boolean specialCharactersExists = false;
+            boolean digitsExists = false;
+            char[] Password = user.getPassword().toCharArray();
+            for (int index = 0; index < user.getPassword().length(); index++) {
+                if (Character.isUpperCase(Password[index]) || Character.isLowerCase(Password[index])) {
+                    alphabetExists = true;
+                } else if (Character.isDigit(Password[index])) {
+                    digitsExists = true;
+                } else if (user.getPassword().contains("~") || user.getPassword().contains("!") || user.getPassword().contains("@")
+                        || user.getPassword().contains("#") || user.getPassword().contains("$") || user.getPassword().contains("%")
+                        || user.getPassword().contains("^") || user.getPassword().contains("&") || user.getPassword().contains("*"))
+                    ;
+                {
+                    specialCharactersExists = true;
+                }
+            }
+                 if ((alphabetExists) && (specialCharactersExists) && (digitsExists )){
+                userService.registerUser(user);
+                 return "users/login";
+            }
+                 else{
+                    String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+                    model.addAttribute("User",user);
+                    UserProfile profile = new UserProfile();
+                     user.setProfile(profile);
+                    user.setUsername("");
+                    model.addAttribute("passwordTypeError", error);
+                    return "users/registration";
+        }
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
